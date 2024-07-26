e90ecf15db7c776d52193c232659cda6dae085d3
dhruvji <dhruvgautam@berkeley.edu>
2024-07-26 23:22:08 +0000

    tests working

--- django/apps/config.py
+++ django/apps/config.py
@@ -6,9 +6,10 @@ from django.core.exceptions import ImproperlyConfigured
 from django.utils.functional import cached_property
 from django.utils.module_loading import import_string, module_has_submodule
 
+from .apps_utils import _path_from_module  # Import the function
+
 APPS_MODULE_NAME = "apps"
 MODELS_MODULE_NAME = "models"
-
 
 class AppConfig:
     """Class representing a Django application and its configuration."""
@@ -44,7 +45,7 @@ class AppConfig:
         # Filesystem path to the application directory e.g.
         # '/path/to/django/contrib/admin'.
         if not hasattr(self, "path"):
-            self.path = self._path_from_module(app_module)
+            self.path = _path_from_module(app_module)  # Use the imported function
 
         # Module containing models e.g. <module 'django.contrib.admin.models'
         # from 'django/contrib/admin/models.py'>. Set by import_models().
@@ -67,34 +68,6 @@ class AppConfig:
     @property
     def _is_default_auto_field_overridden(self):
         return self.__class__.default_auto_field is not AppConfig.default_auto_field
-
-    def _path_from_module(self, module):
-        """Attempt to determine app's filesystem path from its module."""
-        # See #21874 for extended discussion of the behavior of this method in
-        # various cases.
-        # Convert to list because __path__ may not support indexing.
-        paths = list(getattr(module, "__path__", []))
-        if len(paths) != 1:
-            filename = getattr(module, "__file__", None)
-            if filename is not None:
-                paths = [os.path.dirname(filename)]
-            else:
-                # For unknown reasons, sometimes the list returned by __path__
-                # contains duplicates that must be removed (#25246).
-                paths = list(set(paths))
-        if len(paths) > 1:
-            raise ImproperlyConfigured(
-                "The app module %r has multiple filesystem locations (%r); "
-                "you must configure this app with an AppConfig subclass "
-                "with a 'path' class attribute." % (module, paths)
-            )
-        elif not paths:
-            raise ImproperlyConfigured(
-                "The app module %r has no filesystem location, "
-                "you must configure this app with an AppConfig subclass "
-                "with a 'path' class attribute." % module
-            )
-        return paths[0]
 
     @classmethod
     def create(cls, entry):
